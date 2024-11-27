c4b74ab2f567530b1f3aba09d6d1bcff0056221a
abrilvc-quantum <vc223570609@alm.buap.mx>
2024-11-26 20:47:38 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('Física!')
+// pausa
+    .pauseFor(2500)
+    // borra todo
+    .deleteAll()
+    .typeString('Lo cuántico es el futuro')
+    // pausa
+    .pauseFor(2500)
+    // carcateres que se borraran
+    .deleteChars(9)
+    .typeString('<strong>el ahora</strong>')
+    .pauseFor(2500)
+    .start();
